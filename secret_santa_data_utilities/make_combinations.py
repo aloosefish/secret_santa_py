f572bec948b99b5532e08bf6980572108cdad076
Ian Davis <ian.davis@taskray.com>
2023-07-03 15:34:20 -0500

    fix combination algorithm

--- secret_santa_data_utilities/make_combinations.py
+++ secret_santa_data_utilities/make_combinations.py
@@ -1,27 +1,25 @@
 import random
 
-from contact_types import Contact
+from contact_types import Contact, SecretSanta
 
 
-# TODO: Fix this. Currently, this code makes everyone their own
-#  secret santa. With the correct data structure, but not the correct values
 def make_combinations(list_of_contacts: list[Contact]):
-    combos = []
-    selected = []
-    for c in list_of_contacts:
+    selected: list[SecretSanta] = []
+    secret_santas_assigned = list_of_contacts.copy()
+    for i, c in enumerate(list_of_contacts):
         not_me = remove_me(c['name'], list_of_contacts)
         not_me_or_my_spouse = remove_spouse(c, not_me)
         not_selected = remove_already_selected(not_me_or_my_spouse, selected)
-        secret_santa = random.choice(not_selected)
+        choose_one = random.choice(not_selected)
 
-        secret_santa_name = secret_santa['name']
-        secret_santa_phone = secret_santa['phone_number']
-        add_to_combo = {"name": secret_santa_name,
-                        "phone": secret_santa_phone}
-        combos.append(add_to_combo)
-        selected.append(secret_santa)
+        this_secret_santa: SecretSanta = {'name': choose_one['name'],
+                                          'phone_number': choose_one[
+                                              'phone_number']}
 
-    return combos
+        secret_santas_assigned[i]['secret_santa'] = this_secret_santa
+        selected.append(this_secret_santa)
+
+    return secret_santas_assigned
 
 
 def remove_me(me: str, possible_combos: list[dict]):
@@ -40,12 +38,3 @@ def remove_already_selected(list_to_query: list[dict], selected: list[dict]):
 
 def remove_spouse(me: dict, possible_combos: list[dict]):
     return [s for s in possible_combos if s['name'] != me['spouse']]
-
-
-# each contact is their own secret santa
-def inject_secret_santas(secret_santas, contacts) -> list[Contact]:
-    secret_santas_assigned = contacts.copy()
-    for i, contents in enumerate(secret_santas_assigned):
-        secret_santas_assigned[i]['secret_santa'] = secret_santas[i]
-
-    return secret_santas_assigned
--- tests/make_combinations_test.py
+++ tests/make_combinations_test.py
@@ -1,7 +1,8 @@
 import pytest
 
 from secret_santa_data_utilities.make_combinations import remove_me, \
-    remove_spouse
+    remove_spouse, make_combinations
+from contact_types import Contact, SecretSanta
 from create_test_data import create_test_contact_list
 
 
@@ -12,11 +13,19 @@ class TestCombinations:
         return contacts_list
 
     def test_remove_me(self, setup_test_data):
-        # first contact will always have a spouse
         not_me = remove_me(setup_test_data[0]['name'], setup_test_data)
         assert not_me[0]['name'] != setup_test_data[0]['name']
 
     def test_remove_spouse(self, setup_test_data):
+        # first contact will always have a spouse
         not_spouse = remove_spouse(setup_test_data[0],
                                    setup_test_data)
         assert not_spouse[0] != setup_test_data[0]['spouse']
+
+    def test_make_combinations(self, setup_test_data):
+        with_ss_assigned = make_combinations(setup_test_data)
+        assert len(setup_test_data) == len(with_ss_assigned)
+        # every combination is correct
+        for i in with_ss_assigned:
+            if i['secret_santa'] is None:
+                pytest.fail('A contact does not have a secret santa')
