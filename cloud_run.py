6d6d5179864bfbd4e3b4c56cf9af181492eeb9bf
Ian Davis <ian.davis@taskray.com>
2023-10-26 16:16:05 -0500

    debugging cloud_run.py

--- cloud_run.py
+++ cloud_run.py
@@ -3,8 +3,7 @@ import argparse
 
 from src.messages.send_messages import send_text_messages
 from src.data_utilities.get_contacts import get_contacts
-from src.data_utilities.assignment_algorithm import \
-    assign_secret_santa
+from src.data_utilities.assignment_algorithm import assign_secret_santa
 
 parser = argparse.ArgumentParser(prog='SecretSanta',
                                  description='create Secret Santa '
@@ -28,10 +27,12 @@ client = Client(twilio_account_sid, twilio_auth_token)
 
 def main():
     contacts = get_contacts(json_data_url, json_data_key)
+    print(contacts)
     assigned_secret_santas = assign_secret_santa(contacts)
-    send_text_messages(contacts=assigned_secret_santas,
-                       robot_phone_number=twilio_phone_number,
-                       twilio_client=client)
+    print(assigned_secret_santas)
+    # send_text_messages(contacts=assigned_secret_santas,
+    #                    robot_phone_number=twilio_phone_number,
+    #                    twilio_client=client)
 
 
 if __name__ == '__main__':
